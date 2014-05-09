ad0d44e2256981761c8052dd461c55769346fc28
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2014-05-09 18:52:13 -0300

    chore(client.js): fix indentations

--- public/client.js
+++ public/client.js
@@ -21,8 +21,8 @@ var them;
 var draw_at;
 
 var particle = {
-	size: 10,
-	speed: 0.3
+   size: 10,
+   speed: 0.3
 };
 
 var mouseX;
@@ -55,34 +55,34 @@ var sync = true;
 var sendCursorToThem = true;
 
 function send_image(){
-	if(sync){
-		socket.emit("sync_photo_position", {
-			position_x: gesturableImg.position.x,
-			position_y: gesturableImg.position.y,
-			scale_x: gesturableImg.scale.x,
-			scale_y: gesturableImg.scale.y
-		});
-	}
+if(sync){
+   socket.emit("sync_photo_position", {
+      position_x: gesturableImg.position.x,
+      position_y: gesturableImg.position.y,
+      scale_x: gesturableImg.scale.x,
+      scale_y: gesturableImg.scale.y
+   });
+}
 }
 
 function back_video(){
-	location.reload();
+   location.reload();
 }
 
 function hexToRgb(hex) {
-	// Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
-	var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+   // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+   var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
 
-	hex = hex.replace(shorthandRegex, function(m, r, g, b) {
-		return r + r + g + g + b + b;
-	});
+   hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+      return r + r + g + g + b + b;
+   });
 
-	var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-	return result ? {
-		r: parseInt(result[1], 16),
-		g: parseInt(result[2], 16),
-		b: parseInt(result[3], 16)
-	} : null;
+   var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+   return result ? {
+      r: parseInt(result[1], 16),
+      g: parseInt(result[2], 16),
+      b: parseInt(result[3], 16)
+   } : null;
 }
 
 function main_interval(){
